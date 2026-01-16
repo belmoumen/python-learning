16698beafa3964351724a039b60e9ae40ee753b7
belmoumen <belmoumen.meh@gmail.com>
2026-01-16 15:37:08 +0100

    python files

---
+++ Type_casting.py
@@ -0,0 +1,15 @@
+#typecasting is the process of converting a variable from one data to another 
+# str() bool() int() float()
+
+name = "mehdi"
+age = 34
+weight = 95.5
+is_male = True
+
+name = str(name)
+age = str(age)
+age += "1"
+
+print(type(name))
+print(age)
+
---
+++ Variable_input.py
@@ -0,0 +1,29 @@
+#integer
+
+age = 21
+players = 5
+quantity = 5
+
+print("You are", age, "Years old")
+print(f"You are player number {players}")
+print("You have " + str(quantity) + " points")
+
+#FLOAT
+
+htz = 144.5
+result = 9.5
+degree = 8.8
+
+print(f"the screen supports {htz}")
+print(f"the result of the month is {result}")
+print(f"You degree is {degree}")
+
+#STRING
+
+name = "mehdi"
+username = "ceery"
+email = "belmoumen.meh@gmail.com"
+
+print(f"Your name is {name}")
+print(f"Your username is {username}")
+print(f"Your email is {email}")
